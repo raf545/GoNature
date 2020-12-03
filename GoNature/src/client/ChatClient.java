3566630c9e9c53942c4261d15d6c3e898bf0b6ce
dan <dan@DESKTOP-P5VVQ14>
2020-12-03 19:19:43 +0200

    test

--- GoNature/src/client/ChatClient.java
+++ GoNature/src/client/ChatClient.java
@@ -23,6 +23,7 @@ import java.io.*;
  * @Editor Rafael elkoby
  * @version December 3 2020
  */
+ //rafael efes
 
 public class ChatClient extends AbstractClient {
 	// Instance variables **********************************************
